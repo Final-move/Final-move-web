216ed3e9663cb82a6c7ee206556870d1d19922a1
Insigne029 <106772923+Insigne029@users.noreply.github.com>
2023-08-07 23:53:52 +0100

    chores: updates to news section

--- src/pages/home/HomeNews.jsx
+++ src/pages/home/HomeNews.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import PageContainer from "../../components/PageContainer";
-// import NewsCard from "../../components/NewsCard";
+import NewsCard from "../../components/NewsCard";
 
 const BannerNews = () => {
   return (
     <PageContainer className="homeNews-section">
       <section className="home-main-news">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur in
-        repudiandae aliquam natus ipsam pariatur laborum maiores, sit amet
-        nobis, impedit praesentium asperiores corporis labore voluptas expedita
-        provident obcaecati illo deserunt maxime sunt atque porro? Cumque rem
-        odio laboriosam reiciendis adipisci facilis eligendi quasi nihil, illo
-        placeat, cum, recusandae officiis ducimus vel quidem ipsa earum
-        obcaecati iste! Consequuntur, sunt animi! Quaerat quas tenetur molestiae
-        debitis consequatur dolore magnam doloribus qui.
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
       </section>
       <aside className="home-aside-news">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
