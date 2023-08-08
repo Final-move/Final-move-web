fd608ddf61f0adf6f97c1c62cf69cfd601cd4e91
Insigne029 <106772923+Insigne029@users.noreply.github.com>
2023-08-09 00:29:35 +0100

    chores: updates

--- src/App.js
+++ src/App.js
@@ -44,6 +44,7 @@ const App = () => {
           <Route path="/search" element={<Search />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/forgotpassword" element={<Signup />} />
         </Routes>
 
         <Footer />
