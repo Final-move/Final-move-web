2ef4ccf9b0790c9b5f8117be15d7a7bace80ea6a
Insigne029 <106772923+Insigne029@users.noreply.github.com>
2023-08-12 20:23:07 +0100

    Chores: added Blog page

--- src/App.js
+++ src/App.js
@@ -10,8 +10,7 @@ import Search from "./pages/search/Page";
 import Login from "./components/Login";
 import MobileNav from "./components/MobileNav";
 import Signup from "./components/Signup";
-// import Blog from "./pages/blog/Blog";
-// import BlogArticle from "./pages/blog/blog-article/BlogArticle";
+import Blog from "./pages/blog/Page";
 
 export const toggleContext = React.createContext();
 
@@ -37,7 +36,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          {/* <Route path="/blog" element={<Blog />} /> */}
+          <Route path="/blog" element={<Blog />} />
           {/* <Route path="/blog/article/:id" element={<BlogArticle />} /> */}
           <Route path="/resources" element={<Resources />} />
           <Route path="/contact" element={<Contact />} />
