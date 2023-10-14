4b388d1a23aa43dbd57b704c916d884fe9540dac
David Chukwuka Nworie <chukwukadibia029@gmail.com>
2023-10-14 02:34:55 +0100

    chores: update to home

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -7,7 +7,7 @@ const NewsCard = () => {
   const id = 1;
 
   return (
-    <section className="w-full h-[450px] bg-white rounded-lg shadow-md p-2">
+    <section className="w-full h-[450px] bg-white rounded-lg shadow-md p-2 overflow-hidden">
       <Link to={`blog/article/${id}`} className="text-black">
         <div className="w-full overflow-hidden h-[200px] rounded">
           <Img
@@ -16,25 +16,23 @@ const NewsCard = () => {
             className="w-full h-full object-cover object-center hover:scale-110"
           />
         </div>
-        <article className="w-full flex flex-col gap-4 py-4 px-1">
-          <div className="w-full flex items-center gap-6 text-sm font-medium text-yellow-500">
+        <article className="w-full h-[250px] flex flex-col gap-4 py-4 px-1">
+          <div className="w-full flex items-center gap-4 text-sm font-medium text-yellow-500">
             <p className="py-1 px-2 text-yellow-500 bg-black rounded">
               Testimony
             </p>
             <p>August 21, 2017</p>
           </div>
-          <h3 className="text-black text-lg font-semibold">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque,
-            repellendus!
-          </h3>
-          <p className="text-black text-base font-medium">
-            before studying how to do it, let us first of all study how it will
-            not work. often, learning how something does not work helps people
-            not to waste their time on what will not work
-          </p>
-          <p className="text-base font-medium text-yellow-500 underline">
-            Read more
-          </p>
+          <div className="w-full h-full flex flex-col justify-between gap-2 mb-2">
+            <p className="text-black text-lg font-medium">
+              before studying how to do it, let us first of all study how it
+              will not work. often, learning how something does not work helps
+              people not to waste their time on what will not work
+            </p>
+            <p className="text-base font-medium text-yellow-500 underline">
+              Read more
+            </p>
+          </div>
         </article>
       </Link>
     </section>
--- src/pages/home/HomeContact.jsx
+++ src/pages/home/HomeContact.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { PageContainer } from "../../components/general";
+import { Link } from "react-router-dom";
 
 const HomeContact = () => {
   return (
     <PageContainer
       className={"py-20 w-full flex justify-center items-center bg-white"}
     >
-      <div></div>
+      <div className="w-full grid gap-2">
+        <p className="text-xl text-black text-center">
+          We're here to help and connect with you in any way we can.
+        </p>
+
+        <Link
+          to={"/contact"}
+          className="bg-black text-yellow-500 text-base py-2 px-4 rounded-lg text-center"
+        >
+          Plan Your Visit
+        </Link>
+      </div>
     </PageContainer>
   );
 };
