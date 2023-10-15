1409d9a9d5c3ec27722561765be216c929e8eb72
David Chukwuka Nworie <chukwukadibia029@gmail.com>
2023-10-15 08:46:41 +0100

    chores: update to searchcontent

--- src/pages/search/SearchContents.jsx
+++ src/pages/search/SearchContents.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { PageContainer } from "../../components/general";
 import { FaSearch } from "react-icons/fa";
 
 const SearchContents = () => {
+  const [search, setSearch] = useState();
+
   return (
     <PageContainer className={"bg-white w-full min-h-screen py-12"}>
       <div className="w-full max-w-lg mx-auto relative">
+        <label
+          className={
+            search
+              ? `text-xl font-normal bg-transparent text-yellow-500 absolute left-8`
+              : `hidden`
+          }
+        >
+          Search
+        </label>
         <input
           type="text"
-          placeholder="Search"
-          className="w-full h-16 bg-transparent text-black text-xl font-normal p-4 pl-6 border border-yellow-500 rounded-[5rem]"
+          className="w-full h-16 bg-transparent text-black text-xl font-normal p-4 pl-6 border border-yellow-500 hover:border-yellow-500 active:border-yellow-500 rounded-[5rem]"
           autoComplete="off"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
         />
         <button
           type="button"
