2d71b3d84b3151ec37c22742d210fea2b0a5e1ed
David Chukwuka Nworie <chukwukadibia029@gmail.com>
2023-10-16 16:39:11 +0100

    chores: updates

--- src/pages/contact/ContactForm.jsx
+++ src/pages/contact/ContactForm.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { SectionHeader } from "../../components/general";
 import { MoveRight } from "lucide-react";
 
 const ContactForm = () => {
+  const [name, setName] = useState();
+  const [subject, setSubject] = useState();
+  const [message, setMessage] = useState();
+
   const contactSubmit = (e) => e.preventDefault();
 
   return (
@@ -14,21 +18,37 @@ const ContactForm = () => {
 
       <section className="w-full grid gap-4">
         <div className="w-full relative">
-          <label className="absolute top-1/2 -translate-y-1/2 left-4 text-yellow-500 text-base font-medium">
+          <label
+            className={`${
+              name
+                ? "absolute left-4 top-0 -translate-y-1/2 bg-yellow-500 text-black"
+                : "absolute left-4 top-1/2 -translate-y-1/2 text-yellow-500"
+            } "p-2 px-3 text-base font-medium"`}
+          >
             Name
           </label>
           <input
             type="text"
             className="w-full h-12 p-4 bg-transparent border border-yellow-500 rounded-lg"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
         <div className="w-full relative">
-          <label className="absolute top-1/2 -translate-y-1/2 left-4 text-yellow-500 text-base font-medium">
+          <label
+            className={`${
+              subject
+                ? "absolute left-4 top-0 -translate-y-1/2 bg-yellow-500 text-black"
+                : "absolute left-4 top-1/2 -translate-y-1/2 text-yellow-500"
+            } "p-2 px-3 text-base font-medium"`}
+          >
             Subject
           </label>
           <input
             type="text"
             className="w-full h-12 p-4 bg-transparent border border-yellow-500 rounded-lg"
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
           />
         </div>
         <div className="w-full relative">
@@ -38,6 +58,8 @@ const ContactForm = () => {
           <textarea
             type="text"
             className="w-full h-32 p-4 bg-transparent border border-yellow-500 rounded-lg resize-none"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
           />
         </div>
         <div className="place-self-end">
